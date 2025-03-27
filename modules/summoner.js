a9754da919ca87c92e00c96b41f0e5044b931e2c
Dan Brown <danbrown@backtablestudios.com>
2025-03-27 16:40:36 -0700

    Formatting

--- modules/summoner.js
+++ modules/summoner.js
@@ -3,7 +3,7 @@ import { Utils } from "./utils.js";
 export class Summoner {
 
     static async startSummon(summonerToken) {
-        let selectedActorUuid = await game.actorBrowser.openBrowser({actorTypes: ["npc", "character"], initialSourceFilter: "swade-ootd.odyssey-bestiary"});
+        let selectedActorUuid = await game.actorBrowser.openBrowser({ actorTypes: ["npc", "character"], initialSourceFilter: "swade-ootd.odyssey-bestiary" });
         if (!selectedActorUuid) return;
 
         const crosshair = await Sequencer.Crosshair.show({ snap: { resolution: 1 } });
@@ -16,16 +16,16 @@ export class Summoner {
         }
     }
 
-    static async executeSummon(crosshair, sceneId, selectedActorUuid, summonerTokenId, userId) {        
+    static async executeSummon(crosshair, sceneId, selectedActorUuid, summonerTokenId, userId) {
         let summonedActor;
         if (selectedActorUuid.startsWith("Compendium")) {
             summonedActor = await game.tcal.importTransientActor(selectedActorUuid);
         } else {
             summonedActor = fromUuidSync(selectedActorUuid);
         }
-        
+
         if (!summonedActor) return;
-        
+
         let scene = game.scenes.find(s => s.id == sceneId);
         let user = game.users.get(userId);
         let disposition;
@@ -41,7 +41,7 @@ export class Summoner {
         for (let key of Object.keys(summonedActor.ownership)) {
             removedOwnership[key] = 0;
         }
-        
+
         const newTokenDoc = await summonedActor.getTokenDocument({
             x: crosshair.x - scene.grid.size / 2,
             y: crosshair.y - scene.grid.size / 2,
@@ -56,46 +56,46 @@ export class Summoner {
         game.foundryDumpingGround.socket.executeForEveryone("toggleVis", sceneId, createdTokenDoc.id, 0);
 
         let { x, y, width, height } = newTokenDoc;
-            
+
         width *= scene.grid.sizeX;
-        height *= scene.grid.sizeY;       
+        height *= scene.grid.sizeY;
         const newCenterPoint = { x: x + (width / 2), y: y + (height / 2) };
 
         let summonSeq = new Sequence()
-        .effect()
-        .file("jb2a.magic_signs.circle.02.conjuration.intro.yellow")
-        .atLocation(newCenterPoint, {gridUnits: true})
-        .size(newTokenDoc.width * 1.5, { gridUnits: true })
-        .belowTokens();
+            .effect()
+            .file("jb2a.magic_signs.circle.02.conjuration.intro.yellow")
+            .atLocation(newCenterPoint, { gridUnits: true })
+            .size(newTokenDoc.width * 1.5, { gridUnits: true })
+            .belowTokens();
 
         summonSeq.effect()
-        .file("jb2a.particle_burst.01.circle.yellow")
-        .atLocation(newCenterPoint, {gridUnits: true})
-        .size(newTokenDoc.width * 2, { gridUnits: true })
-        .delay(1300)
-        .belowTokens(false);
-        
+            .file("jb2a.particle_burst.01.circle.yellow")
+            .atLocation(newCenterPoint, { gridUnits: true })
+            .size(newTokenDoc.width * 2, { gridUnits: true })
+            .delay(1300)
+            .belowTokens(false);
+
         summonSeq.animation()
-        .on(createdTokenDoc)
-        .opacity(1)
-        .fadeIn(50)
-        .delay(2300)
-        .wait(3500)
-        .thenDo(function () {
-            game.foundryDumpingGround.socket.executeForEveryone("toggleVis", sceneId, createdTokenDoc.id, 1);
+            .on(createdTokenDoc)
+            .opacity(1)
+            .fadeIn(50)
+            .delay(2300)
+            .wait(3500)
+            .thenDo(function () {
+                game.foundryDumpingGround.socket.executeForEveryone("toggleVis", sceneId, createdTokenDoc.id, 1);
 
-            let displayBars = createdTokenDoc.actor.system.wounds.max > 0 ? CONST.TOKEN_DISPLAY_MODES.ALWAYS : CONST.TOKEN_DISPLAY_MODES.NONE;
+                let displayBars = createdTokenDoc.actor.system.wounds.max > 0 ? CONST.TOKEN_DISPLAY_MODES.ALWAYS : CONST.TOKEN_DISPLAY_MODES.NONE;
 
-            let updateData = {
-                displayBars: displayBars,
-                disposition: disposition,
-                delta: { ownership: {} },
-            };
+                let updateData = {
+                    displayBars: displayBars,
+                    disposition: disposition,
+                    delta: { ownership: {} },
+                };
 
-            updateData.delta.ownership[userId] = CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER;
+                updateData.delta.ownership[userId] = CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER;
 
-            createdTokenDoc.update(updateData);
-        })
+                createdTokenDoc.update(updateData);
+            });
 
         summonSeq.play();
     }
